6f92738772def2bfbd3140b525f9a07a00022cc7
Cax36940 <115898972+Cax36940@users.noreply.github.com>
2024-10-15 19:53:15 -0400

    Add SpringSprite
    This is a simple line just to see the spring

---
+++ PhysicsEngine/src/Component/Graphics/SpringSprite.cpp
@@ -0,0 +1,27 @@
+#include <ofGraphics.h>
+#include "SpringSprite.hpp"
+
+
+void SpringSprite::set_width(const float& new_width) {
+	width = new_width;
+}
+
+void SpringSprite::set_color(const float& r, const float& g, const float& b) {
+	color = glm::vec3(r, g, b);
+}
+
+void SpringSprite::set_positions(const Vector3& pos1, const Vector3& pos2) {
+	point1 = pos1;
+	point2 = pos2;
+
+}
+
+void SpringSprite::draw()
+{
+	if (!visible) {
+		return;
+	}
+	ofSetColor(color.r, color.g, color.b);
+	ofSetLineWidth(width);
+	ofDrawLine(Vector3::to_glm_vec3(point1), Vector3::to_glm_vec3(point2));
+}
---
+++ PhysicsEngine/src/Component/Graphics/SpringSprite.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Component/Graphics/GraphicsComponent.hpp"
+#include "Component/Vector3.hpp"
+
+
+class SpringSprite : public GraphicsComponent
+{
+private:
+	Vector3 point1;
+	Vector3 point2;
+	float width;
+	glm::vec3 color;
+
+public:
+	SpringSprite() = default;
+	SpringSprite(const SpringSprite&) = default;
+	SpringSprite& operator=(const SpringSprite&) = default;
+	SpringSprite(const Vector3& pos1, const Vector3& pos2, const float& width, const glm::vec3& color) : point1(pos1), point2(pos2), width(width), color(color) {}
+
+
+	void set_width(const float& new_width);
+	void set_color(const float& r, const float& g, const float& b);
+	void set_positions(const Vector3& pos1, const Vector3& pos2);
+
+	void draw() override;
+};
