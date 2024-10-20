9dd94750afc218ba633a1615f9d889c516a5cf71
Cax36940 <115898972+Cax36940@users.noreply.github.com>
2024-10-20 18:48:30 -0400

    Move SpringSprite to Line

--- PhysicsEngine/src/Component/Graphics/Line.cpp
+++ PhysicsEngine/src/Component/Graphics/Line.cpp
@@ -1,21 +1,21 @@
 #include <ofGraphics.h>
-#include "SpringSprite.hpp"
+#include "Line.hpp"
 
-void SpringSprite::set_width(const float& new_width) {
+void Line::set_width(const float& new_width) {
 	width = new_width;
 }
 
-void SpringSprite::set_color(const float& r, const float& g, const float& b) {
+void Line::set_color(const float& r, const float& g, const float& b) {
 	color = glm::vec3(r, g, b);
 }
 
-void SpringSprite::set_positions(const Vector3& pos1, const Vector3& pos2) {
+void Line::set_positions(const Vector3& pos1, const Vector3& pos2) {
 	position_a = pos1;
 	position_b = pos2;
 
 }
 
-void SpringSprite::draw()
+void Line::draw()
 {
 	if (!visible) {
 		return;
