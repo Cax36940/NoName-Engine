fa5767d5dc31c3dff5a16653a748d2a21e84a499
Cax36940 <115898972+Cax36940@users.noreply.github.com>
2024-10-24 22:07:26 -0400

    TO merge with previous

--- PhysicsEngine/src/Component/Graphics/Line.cpp
+++ PhysicsEngine/src/Component/Graphics/Line.cpp
@@ -1,18 +1,18 @@
 #include <ofGraphics.h>
 #include "Line.hpp"
 
+void Line::set_positions_ptr(const Vector3* pos1, const Vector3* pos2)
+{
+	position_a = pos1;
+	position_b = pos2;
+}
+
 void Line::set_width(const float& new_width) {
 	width = new_width;
 }
 
 void Line::set_color(const float& r, const float& g, const float& b) {
 	color = glm::vec3(r, g, b);
-}
-
-void Line::set_positions(const Vector3& pos1, const Vector3& pos2) {
-	position_a = pos1;
-	position_b = pos2;
-
 }
 
 void Line::draw()
@@ -22,5 +22,5 @@ void Line::draw()
 	}
 	ofSetColor(color.r, color.g, color.b);
 	ofSetLineWidth(width);
-	ofDrawLine(Vector3::to_glm_vec3(position_a), Vector3::to_glm_vec3(position_b));
+	ofDrawLine(Vector3::to_glm_vec3(*position_a), Vector3::to_glm_vec3(*position_b));
 }
